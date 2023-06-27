9ff4649879a5e9d97a098a6f3ae11755d0d7ac75
Paul <pmacellaro7@aol.com>
2023-06-27 15:51:52 -0400

    Component changes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Switch, Route } from "react-router-dom"
-
+import { Switch, Route } from "react-router-dom" 
 import NavBar from './NavBar';
 import DrinkContainer from './DrinkContainer';
 import Header from './Header';
