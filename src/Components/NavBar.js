ce65b8d9f3b8475e32011e0158c080b7fd8bdd22
Sadaf Chadwick <sadafchadwick@gmail.com>
2023-06-27 14:17:26 -0500

    another update to navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,25 +5,21 @@ function NavBar() {
 
   return (
     <nav>
-        <a href="#animation animation start-about">HOME
-          <NavLink to="/"></NavLink>
-        </a>
-        <a href="#animation start-blog">COCKTAILS
-          <NavLink to="/cocktails"></NavLink>
-        </a>
-        {/* </div> */}
-        {/* <div class="animation start-blog"> */}
-        <a href="#animation start-portefolio">MOCKTAILS
-          <NavLink to="/mocktails"></NavLink>
-        </a>
-        {/* </div> */}
-        {/* <div class="animation start-portefolio"> */}
-        <a href="#animation start-contact">MY RECIPE BOOK
-          <NavLink to="/myrecipebook"></NavLink>
-        </a>
-          <div class="animation start-home"></div>
-      </nav>
- 
+      <a href="#animation animation start-about">HOME
+        <NavLink to="/"></NavLink>
+      </a>
+      <a href="#animation start-blog">COCKTAILS
+        <NavLink to="/cocktails"></NavLink>
+      </a>
+      <a href="#animation start-portefolio">MOCKTAILS
+        <NavLink to="/mocktails"></NavLink>
+      </a>
+      <a href="#animation start-contact">MY RECIPE BOOK
+        <NavLink to="/myrecipebook"></NavLink>
+      </a>
+      <div class="animation start-home"></div>
+    </nav>
+
   )
 }
 
