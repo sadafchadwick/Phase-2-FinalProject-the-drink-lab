338fa4bfa9b3518b989a9d5c68b8e9eeb5e49f34
Sadaf Chadwick <sadafchadwick@gmail.com>
2023-06-29 11:18:31 -0500

    styling of header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,9 @@ import logo from "../carouselpics/logofordrinklab.jpg"
 function Header(){
     return(
     <>
-        <img className="cocktailLab" src= {logo} alt="Logo for Drink Lab" style={{align: "center"}}/>
         <span>
-        <h1 className="mainHeader" style={{fontSize: "75px"}}>Welcome to Our Drink Lab!</h1>
+        <img className="cocktailLab" src= {logo} alt="Logo for Drink Lab" style={{align: "center", float:"left", height: "120px", padding: "15px"}}/>
+        <h1 className="mainHeader" style={{fontSize: "65px", padding:"25px"}}>Welcome to Our Drink Lab!</h1>
         </span>
         <h3 className="secondHeader">Created by Sadaf Chadwick, Paul Macellaro, and Amelia Freeman</h3>
     </>
